919950989d11b36e1422ab15363527715600b27d
nikolapivac <npivac00@fesb.hr>
2022-07-07 12:29:48 +0200

    Passed test 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = "";
+    if(num<0)
+        result = "ERROR"
+    for(let i=0; i<num; i++)
+        result = result.concat(str)
+    return result;
 };
 
 // Do not edit below this line
