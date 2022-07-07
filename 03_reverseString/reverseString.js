c47c24649cdc0140b1a97f6e85ccff7f97c45769
nikolapivac <npivac00@fesb.hr>
2022-07-07 12:49:44 +0200

    Passed test 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let len = string.length;
+    string = string.split("");
+    string = Array.from(string)
+    let result = [];
+    for(let i=0; i<len; i++){
+        result[i] = string[len-1-i]
+    }
+    result = result.join("")
+    return result;
 };
 
 // Do not edit below this line
