c3004bcd0d34bcbc927d9e9c1b07864482752b34
nikolapivac <npivac00@fesb.hr>
2022-07-07 13:19:10 +0200

    Passed test 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) {
+    let len = array.length;
+    let result = []
+    for (let j=1; j<arguments.length; j++){
+        for (let i=0;i<len;i++){
+            if(array[i] == arguments[j]){
+                result = array.splice(i, 1);
+            }
+        }
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
