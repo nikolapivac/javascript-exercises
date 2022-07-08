26d2c6c592bb04a98e8d578b405dde59a63c97cd
nikolapivac <npivac00@fesb.hr>
2022-07-08 18:53:48 +0200

    Passed test 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    const array = Array.from(string);
+    const clean = array.filter(e => {
+        return (e != " " && e != "!" && e != "?" && e!= "," && e!=".") ? true : false;
+    })
 
+    let len = clean.length;
+    let result = [];
+    for(let i = 0; i < len; i++){
+        result[i] = clean[len-1-i];
+    }
+
+    let a = clean.toString();
+    let b = result.toString();
+
+    if(a==b) {
+        return true;
+    } else return false;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
