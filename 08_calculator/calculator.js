ff5aae3e2543d08a7c698448cc9b93a84b319a66
nikolapivac <npivac00@fesb.hr>
2022-07-08 18:20:26 +0200

    Passed test 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let len = array.length;
+  let s = 0;
+  for(let i = 0; i < len; i++){
+    s += array[i];
+  }
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let len = array.length;
+  let s = 1;
+  for(let i = 0; i < len; i++){
+    s *= array[i];
+  }
+  return s;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let s = 1;
+  if (a == 0){
+    return 1;
+  } else{
+    for(let i = 1; i <= a; i++){
+      s *= i;
+    }
+  }
+	return s;
 };
 
 // Do not edit below this line
