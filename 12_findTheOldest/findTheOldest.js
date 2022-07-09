6bd20cf68c816959558d0f2140344476de3fdf72
nikolapivac <npivac00@fesb.hr>
2022-07-09 11:12:47 +0200

    Passed test 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let len = people.length;
 
+    const yearCalc = people.map(e => {
+        if (!("yearOfDeath" in e)){
+            const d = new Date();
+            e.years = d.getFullYear() - e.yearOfBirth;
+        }   
+        else{
+            e.years = e.yearOfDeath - e.yearOfBirth;
+        }
+        return e;
+    });
+
+    const sorted = yearCalc.sort((a, b) => { 
+        return a.years > b.years ? 1 : -1;
+    });
+
+    return sorted[len-1];
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
