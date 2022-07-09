856c0fb03ef6f111e52ce4de89a3157da0b42992
nikolapivac <npivac00@fesb.hr>
2022-07-09 10:29:36 +0200

    Passed test 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const result = books.map(e => e.title
+    );
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
